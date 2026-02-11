ec004703efd6311a1e9149454e5ed340aecc7624
Sangamesh M Bacha <106047704+Sangameshmbacha@users.noreply.github.com>
2026-02-11 12:31:34 +0530

    Remove commented scanBasePackages from application class
    Removed commented-out scanBasePackages configuration.

--- src/main/java/com/example/EmployeeServicesApplication.java
+++ src/main/java/com/example/EmployeeServicesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//(scanBasePackages = "com.example")
 public class EmployeeServicesApplication {
 
 	public static void main(String[] args) {
